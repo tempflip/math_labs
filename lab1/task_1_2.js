a82af7a979ebbd559a6f6e18a61d48722bfd979e
Peter Tempfli <tempflip@gmail.com>
2020-02-13 08:30:42 +0100

    w

--- lab1/task_1_2.js
+++ lab1/task_1_2.js
@@ -12,25 +12,6 @@ const fib_req = (n) => {
 	return fib_req(n-1) + fib_req(n-2);
 }
 
-const fib_iter_list = (n) => {
-	var x = [0, 1]
-	while (x.length <= n) {
-		x.push(x[x.length -1] + x[x.length -2])
-	}
-	return x;
-}
-
-const investigate_last2 = (l) => {
-	var myMap = {};
-	for (i = 0; i <= l.length; i++) {
-		
-		if (!myMap[i] ) {
-			myMap[i] = [];
-		}
-	}
-
-	console.log(myMap);
-}
 
 const b = 40;
 
@@ -44,12 +25,6 @@ console.log('\n\n### Running the recursive Fibanocci algorithm for n = ' + b);
 var t = Date.now();
 console.log(fib_req(b));
 console.log('fib_req time: ' , Date.now() - t);
-
-// var last2 = fib_iter_list(1000)
-// 					.map( x => x % 100 );
-
-
-// investigate_last2([1,2,3,4,1,2,3,4]);
 
 
 
