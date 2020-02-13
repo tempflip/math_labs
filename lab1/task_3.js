ebdac20b1c540e5aad495f94f1300bc327370499
Peter Tempfli <tempflip@gmail.com>
2020-02-13 08:44:11 +0100

    w

--- lab1/task_3.js
+++ lab1/task_3.js
@@ -6,39 +6,42 @@ const fib_iter_list = (n) => {
 	return x;
 }
 
+/*
+* Gets a list of elements, and returns a list of indices from where the list repeats itself
+* If no repeats in the lists, returns an empty list
+*/
 const ifPer = (l) => {
 
-	var patList = [];
-	
+	var indexList = []; // the list of indices to return
 	var pat = [];
 
-	for (var i = 0; i < l.length; i ++) {
-		pat.push(l[i]);
-		pc = 0;
-		var goodPattern = true;
-		for (var c = 0; c < l.length; c ++) {
+	for (var i = 0; i < l.length; i ++) { // a loop which goes through the list
+		pat.push(l[i]); // putting the current element into the current 'pattern candidate'
+
+		var goodPattern = true; // if it is a 'good pattern, it repeats itself in the list;
+		pc = 0; // a counter for the pattern candidate
+		for (var c = 0; c < l.length; c ++) { // an inner loop which figures out of there is a repition
 			if (pc >= pat.length) pc = 0;
 
-			if (pat[pc] != l[c]) {
+			if (pat[pc] != l[c]) { // if there is no match between the pattern and the element of the list, it is not a matching pattern
 				goodPattern = false;
 				break;
 			}
 			pc ++;
 		}
-		if (goodPattern && pat.length <= l.length / 2) {
-			patList.push(pat.length);
+		if (goodPattern && pat.length <= l.length / 2) { // if it is a mathcing pattern, we record the index
+			indexList.push(pat.length);
 		}
 
 	}
-	return patList;
+	return indexList;
 }
 
-const l = [1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3];
+// const l = [1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3];
 
-console.log('Test for a trivial list: ')
-console.log(l);
-console.log('Repeats:');
-console.log(ifPer(l));
+// console.log('Test for a trivial list: ')
+// console.log('Repeats:');
+// console.log(ifPer(l));
 
 
 last1 = fib_iter_list(1000).map( el => el % 10n);
@@ -52,6 +55,4 @@ console.log('Last 2 digit repeats every N times:');
 console.log(ifPer(last2));
 console.log('Last 3 digit repeats every N times:');
 console.log(ifPer(last3));
-// console.log(JSON.stringify(last2))
-// console.log(fib_iter_list(1000).slice(900, 1000))
 
