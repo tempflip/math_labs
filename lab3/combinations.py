12d91a1fb05b8550895eca4f1dd9002cfe358244
Peter Tempfli <tempflip@gmail.com>
2020-07-03 22:52:32 +0200

    w

---
+++ lab3/combinations.py
@@ -0,0 +1,25 @@
+def get_orders(drinks = 3, people = 4):
+	if (people == 1):
+		orders = []
+		for i in range(drinks):
+			orders.append([i])
+		return orders
+	else :
+		orders = []
+		for order in get_orders(drinks, people-1):
+			for i in range(drinks):
+				this_order = order.copy()
+				this_order.append(i)
+				orders.append(this_order)
+
+		return orders
+
+
+print ('If order do matter number of possible orders (simple repetition):')
+print (len(get_orders(3,4)))
+print ('If order doesnt matter number of possible orders (combination with repetetion):')
+print(len(set([tuple(sorted(o)) for o in get_orders(3,4)])))
+
+
+
+		
